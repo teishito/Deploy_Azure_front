3712b497b7d3a6ee5e6d55d357b218f496b99d67
teishito <shito.tei@foru.co.jp>
2024-12-05 01:00:13 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
   const [getResponse, setGetResponse] = useState('');
 
   const handleGetRequest = async () => {
-    const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/hello', {
+    const res = await fetch('https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/hello', {
+    //const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/hello', {
       method: 'GET',
     });
     const data = await res.json();
@@ -26,7 +27,8 @@ export default function Home() {
   const handleIdRequest = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/multiply/${id}`, {
+    const res = await fetch(`https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/multiply/${id}`, {
+    //const res = await fetch(`https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/multiply/${id}`, {
       method: 'GET',
     });
     const data = await res.json();
@@ -47,7 +49,8 @@ export default function Home() {
     //入力されたデータをコンソールに表示
     console.log("入力情報:", input);
 
-    const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/echo', {
+    const res = await fetch('https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/echo', {
+    //const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/echo', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -72,7 +75,8 @@ export default function Home() {
   const handleTranslateSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/translate', {
+    const res = await fetch('https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/translate', {
+    //const res = await fetch('https://tech0-gen-8-step3-testapp-py2-19.azurewebsites.net/api/translate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

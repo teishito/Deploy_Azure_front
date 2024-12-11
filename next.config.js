a10a6884a2102a8c21706de7230ced96890839df
teishito <shito.tei@foru.co.jp>
2024-12-11 11:31:04 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-};
-
-module.exports = nextConfig
-module.exports = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/:path*' // バックエンドのURL
+        destination: 'https://tech0-gen-8-step3-app-py-10.azurewebsites.net/api/:path*', // バックエンドのURL
       },
     ];
   },
 };
+
+module.exports = nextConfig;

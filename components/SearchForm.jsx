faa2a9663b5e9630d823919eb0c30f695ce19f43
teishito <shito.tei@foru.co.jp>
2024-12-11 13:06:59 +0900

    Update SearchForm.jsx

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 export default function SearchForm() {
   const [area, setArea] = useState(""); // エリア

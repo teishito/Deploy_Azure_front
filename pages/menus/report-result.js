b1fc83a67a73905ce99d5861244333f4808e7f10
teishito <shito.tei@foru.co.jp>
2024-12-11 12:53:00 +0900

    Update report-result.js

--- pages/menus/report-result.js
+++ pages/menus/report-result.js
@@ -1,1 +1,10 @@
 import Header from "../../components/Header";
+
+export default function ReportResult() {
+  return (
+    <div>
+      <h1>Report Result Page</h1>
+      <p>こちらはレポート結果のページです。</p>
+    </div>
+  );
+}

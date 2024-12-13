f836cfd34722bac11fa0d95a9e10e11501155a0b
teishito <shito.tei@foru.co.jp>
2024-12-13 15:25:29 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center">
+        <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center shadow-md">
             <p className="text-sm text-gray-500">© 2024 FortuneDinner. All rights reserved.</p>
         </footer>
     );

9b418b01305297168b60bd68eac24a716b81594d
teishito <shito.tei@foru.co.jp>
2024-12-16 13:34:25 +0900

    Update Ad.jsx

--- components/Ad.jsx
+++ components/Ad.jsx
@@ -2,7 +2,10 @@ export default function Ad() {
   const footerImageUrl =
     "https://prcdn.freetls.fastly.net/release_image/17692/682/17692-682-f34e9af01af09312c77d588504087517-1075x650.jpg?width=1950&height=1350&quality=85%2C75&format=jpeg&auto=webp&fit=bounds&bg-color=fff";
   const footerLink = "https://prtimes.jp/main/html/rd/p/000000682.000017692.html";
-
+  const footerImageUrl2 =
+    "https://prcdn.freetls.fastly.net/release_image/17692/677/17692-677-a1eab6c4884971af38d96d01f6761985-2048x1365.jpg?width=1950&height=1350&quality=85%2C65&format=jpeg&auto=webp&fit=bounds&bg-color=fff";
+  const footerLink2 = "https://prtimes.jp/main/html/rd/p/000000677.000017692.html";
+  
   return (
     <div
       style={{
@@ -15,6 +18,13 @@ export default function Ad() {
       <a href={footerLink} target="_blank" rel="noopener noreferrer">
         <img
           src={footerImageUrl}
+          alt="Ad Image"
+          style={{ height: "200px", margin: "0 auto" }}
+        />
+      </a>
+      <a href={footerLink2} target="_blank" rel="noopener noreferrer">
+        <img
+          src={footerImageUrl2}
           alt="Ad Image"
           style={{ height: "200px", margin: "0 auto" }}
         />
